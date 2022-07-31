dc0a8ff87fbb385fed40d00d32908565ec86eb76
AbduljabbarSal <asalk025@uottawa.ca>
2022-07-31 10:07:09 -0400

    Fix

--- src/App.js
+++ src/App.js
@@ -6,22 +6,22 @@ import Pomodoro from './Pomodoro';
 import Support from './Support';
 import About from './About';
 import {Route, Routes, Navigate} from 'react-router-dom';
-//make all routes start with /DoListo . not gon wrok
+//make all routes start with /DoListo . not gon
 function App() {
   
   return (
     <>
-      <Navbar/>
 
       <div className="App">
+      <Navbar/>
       
         <Routes>
         <Route path="/" element={<Navigate replace to="/About" />} />
         <Route path="/DoListo" element={<Navigate replace to="/About" />} />
-        <Route path = "/About" element={<Navigate replace to="/About"/>}></Route>
-        <Route path = "/TodoList" element={<Navigate replace to="/TodoList"/>}></Route>
-        <Route path = "/Pomodoro" element={<Navigate replace to="/Pomodoro"/>}></Route>
-        <Route path = "/Support" element={<Navigate replace to="/Support"/>}></Route>
+        <Route path = "/About" element={<About></About>}></Route>
+        <Route path = "/TodoList" element={<TodoList></TodoList>}></Route>
+        <Route path = "/Pomodoro" element={<Pomodoro></Pomodoro>}></Route>
+        <Route path = "/Support" element={<Support></Support>}></Route>
         </Routes>
 
       </div>
