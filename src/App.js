ddf7a59e3a62f51d69addf1a159f56478560fdab
AbduljabbarSal <asalk025@uottawa.ca>
2022-07-31 09:49:09 -0400

    approved fix

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Pomodoro from './Pomodoro';
 import Support from './Support';
 import About from './About';
 import {Route, Routes, Navigate} from 'react-router-dom';
-
+//make all routes start with /DoListo . not gon wrok
 function App() {
   
   return (
@@ -18,10 +18,10 @@ function App() {
         <Routes>
         <Route path="/" element={<Navigate replace to="/About" />} />
         <Route path="/DoListo" element={<Navigate replace to="/About" />} />
-        <Route path = "/About" element={<About></About>}></Route>
-        <Route path = "/TodoList" element={<TodoList></TodoList>}></Route>
-        <Route path = "/Pomodoro" element={<Pomodoro></Pomodoro>}></Route>
-        <Route path = "/Support" element={<Support></Support>}></Route>
+        <Route path = "/About" element={<Navigate replace to="/About"/>}></Route>
+        <Route path = "/TodoList" element={<Navigate replace to="/TodoList"/>}></Route>
+        <Route path = "/Pomodoro" element={<Navigate replace to="/Pomodoro"/>}></Route>
+        <Route path = "/Support" element={<Navigate replace to="/Support"/>}></Route>
         </Routes>
 
       </div>
