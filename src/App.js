69b09b7cdd1e4cec25616309a879d2b51da617a0
AbduljabbarSal <asalk025@uottawa.ca>
2022-08-07 05:30:50 -0400

    back to original implementation of routing without using React Router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+/*import React from 'react';
 import './index.css';
 import Navbar from './Navbar';
 import TodoList from './TodoList';
@@ -25,6 +25,51 @@ function App() {
       </div>
     </HashRouter>
   </div>
+  
+  );
+}
+export default App;
+*/
+import React from 'react';
+import './index.css';
+import Navbar from './Navbar';
+import TodoList from './TodoList';
+import Pomodoro from './Pomodoro';
+import Support from './Support';
+import About from './About';
+
+function App(props,state) {
+  let Component;
+  switch(window.location.pathname){
+    
+    case "/":
+      Component = About
+      break
+  
+    case "/DoListo":
+      Component = About
+      break
+
+    case "/TodoList":
+      Component = TodoList
+      break
+    case "/Pomodoro":
+      Component = Pomodoro
+      break
+    case "/Support":
+      Component = Support
+      break
+    case "/About":
+      Component = About
+      break  
+  }
+  
+  return (
+    <div className="App">
+
+      <Navbar/>
+      <Component></Component>
+    </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,13 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import {HashRouter as Router} from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router basename="/">
       <App />
-    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
